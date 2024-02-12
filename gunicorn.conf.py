da56458c91dcf606366269662dc5179343145b02
Intelligencia-fr <yehia.taher@intelligencia.fr>
2024-02-12 12:21:34 +0200

    updated gitignore

---
+++ gunicorn.conf.py
@@ -0,0 +1,15 @@
+# Gunicorn configuration file
+import multiprocessing
+
+max_requests = 1000
+max_requests_jitter = 50
+
+timeout = 100
+graceful_timeout = 20
+
+log_file = "-"
+
+bind = "0.0.0.0:3100"
+
+worker_class = "uvicorn.workers.UvicornWorker"
+workers = (multiprocessing.cpu_count() * 2) + 1
